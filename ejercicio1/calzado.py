06e06dc84e7b932e77b695f0e12726fad69d5c17
EnzoPor98 <127218831+EnzoPor98@users.noreply.github.com>
2026-04-14 05:20:26 -0300

    Puntos_A_B_F_G

--- ejercicio1/calzado.py
+++ ejercicio1/calzado.py
@@ -12,3 +12,29 @@ class Calzado:
         self.color: CalzadoColor
         self._cantidad : int  # Cantidad de Existencias de este producto
         self._precio : float
+        
+    #GETTERS
+    @property
+    def sku(self) -> int:
+        return self._sku
+    
+    @property
+    def cantidad(self) -> int:
+        return self._cantidad
+    
+    @property
+    def precio(self) -> float:
+        return self._precio
+    
+    #SETTERS
+    @sku.setter
+    def sku(self, sku: int) -> None:
+        self._sku = sku
+
+    @cantidad.setter
+    def cantidad(self, cantidad: int) -> None:
+        self._cantidad = cantidad
+
+    @precio.setter
+    def precio(self, precio: float) -> None:
+        self._precio = precio
