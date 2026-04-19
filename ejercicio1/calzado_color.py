5a804ca0d953d6a2d37dcef392aa6c9f6ac91116
Nahuel Montenegro <enimonte20@gmail.com>
2026-04-18 23:02:39 -0300

    Correcciones y prueba de métodos en main

--- ejercicio1/calzado_color.py
+++ ejercicio1/calzado_color.py
@@ -1,12 +1,12 @@
 class CalzadoColor:
-    def __init__(self, primario: str, secundario: str) -> None:
-        self.primario: str
-        self.secundario: str | None # NO HACE OPCIONAL AL PARAMETRO.
+    def __init__(self, primario: str, secundario: str | None) -> None:
+        self.primario = primario
+        self.secundario = secundario
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, CalzadoColor) and self.primario == other.primario and self.secundario == other.secundario
-    
-    def __str__(self, ) -> str:
+
+    def __str__(self) -> str:
         return (f"CalzadoColor(Primario: {self.primario}, Secundario: {self.secundario})")
     
     def __repr__(self):
