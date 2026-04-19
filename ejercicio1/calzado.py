31f431d2e86e782fad4ff0e8f1b60cc06b047ad7
Nahuel Montenegro <enimonte20@gmail.com>
2026-04-18 22:18:13 -0300

    Corrección de calzado, constructores parametrizados, metodo total

--- ejercicio1/calzado.py
+++ ejercicio1/calzado.py
@@ -4,18 +4,36 @@ from calzado_talle import CalzadoTalle
 
 
 class Calzado:
-    def __init__(self):
-        self._sku: int
-        self.nombre: str
-        self.descripcion: str
-        self.tipo: CalzadoTipo
-        self.talle: CalzadoTalle
-        self.color: CalzadoColor
-        self._cantidad: int  # Cantidad de Existencias de este producto
-        self._precio: float
+    def __init__(self, sku: int, nombre: str, descripcion: str, tipo: CalzadoTipo, talle: CalzadoTalle, color: CalzadoColor, cantidad: int, precio: float):
+        if sku < 0:
+            raise ValueError("Sku no puede ser un valor negativo")
+        self._sku = sku
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.tipo = tipo
+        self.talle = talle
+        self.color = color
+        if cantidad < 0:
+            raise ValueError("La cantidad no puede ser un valor negativo")
+        self._cantidad = cantidad
+        if precio < 0:
+            raise ValueError("El precio no puede ser un valor negativo")
+        self._precio = precio
 
+    @property
     def total(self) -> float:
-        pass
+        return self._cantidad * self._precio
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Calzado):
+            return False
+        return self._sku == other._sku
+
+    def __str__(self) -> str:
+        return f"{self.nombre} - {self.tipo} - Talle: {self.talle} - Color: {self.color}"
+
+    def __repr__(self) -> str:
+        return f"Calzado(sku={self._sku}, nombre='{self.nombre}', descripcion='{self.descripcion}', tipo={repr(self.tipo)}, talle={repr(self.talle)}, color={repr(self.color)}, cantidad={self._cantidad}, precio={self._precio})"
 
     # GETTERS
     @property
